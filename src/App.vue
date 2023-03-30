94037748cef81b87263ea1eb1589720eaf2a5b4d
andres felipe vargas <anmendez845@gmail.com>
2023-03-30 17:02:29 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NintendoApp />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NintendoApp from './components/NintendoApp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NintendoApp
   }
 }
 </script>
